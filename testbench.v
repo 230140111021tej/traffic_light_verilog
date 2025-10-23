d8a923587b8e44468d18a992b1bc97a252e1989b
Mallah Tejas Rajkumar <230140111020@gtu.edu.in>
2025-10-23 20:59:27 +0530

    Update testbench.v

--- testbench.v
+++ testbench.v
@@ -1,20 +1,30 @@
-//testbench
 `include "traffic.v"
+
 module tb1;
-reg clock,in;
-wire [2:0]hwy; wire[2:0]normal;
-trafficlight DUT(clock,in,hwy,normal);
-always #10 clock = ~clock;
-initial
-begin
-$dumpfile("traffic.vcd");
-$dumpvars(0,tb1);
-$monitor ($time,"clock=%b,in=%b,hwy=%b,normal=%b",clock,in,hwy,normal);
-clock =1'b0; in=1'b0;
-#50 in = 1'b1;
-#100 in = 1'b0;
-#160 in = 1'b1;
-#30 in=1'b0;
-#30 $finish;
-end
+    reg clock, in;                  // clock: simulation clock, in: car arrival
+    wire [2:0] hwy;                 // highway lights
+    wire [2:0] normal;              // normal road lights
+
+    // Instantiate DUT
+    trafficlight DUT(clock, in, hwy, normal);
+
+    // Generate clock pulse every 10 time units
+    always #10 clock = ~clock;
+
+    initial begin
+        $dumpfile("traffic.vcd");
+        $dumpvars(0, tb1);
+
+        $monitor($time, " clock=%b, in=%b, hwy=%b, normal=%b", clock, in, hwy, normal);
+
+        // Initial conditions
+        clock = 1'b0; in = 1'b0;
+
+        // Stimulus: simulate car arrival and departures at various intervals
+        #50  in = 1'b1;    // Car arrives on normal road
+        #100 in = 1'b0;    // Car departs
+        #160 in = 1'b1;    // Another car arrives
+        #30  in = 1'b0;    // Car departs
+        #30  $finish;      // End simulation
+    end
 endmodule
